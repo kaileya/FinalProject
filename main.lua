e660ebb956fb3fe19f7f2d89106b33de7d22e60a
kaileya <149716331+kaileya@users.noreply.github.com>
2023-12-11 23:38:15 -0500

    Update main.lua

--- main.lua
+++ main.lua
@@ -1,20 +1,12 @@
 -- https://github.com/Ulydev/push
 push = require 'push'
 
--- the "Class" library we're using will allow us to represent anything in
--- our game as code, rather than keeping track of many disparate variables and
--- methods
 Class = require 'class'
 
--- our Paddle class, which stores position and dimensions for each Paddle
--- and the logic for rendering them
 require 'Paddle'
 
--- our Ball class, which isn't much different than a Paddle structure-wise
--- but which will mechanically function very differently
 require 'Ball'
 
--- Brick class
 require 'Brick'
 
 WINDOW_WIDTH = 1280
@@ -23,75 +15,52 @@ WINDOW_HEIGHT = 720
 VIRTUAL_WIDTH = 432
 VIRTUAL_HEIGHT = 243
 
--- speed at which we will move our paddle; multiplied by dt in update
 PADDLE_SPEED = 200
-
---[[
-    Runs when the game first starts up, only once; used to initialize the game.
-]]
-
-
 
 
 function love.load()
     
-    -- set love's default filter to "nearest-neighbor", which essentially
-    -- means there will be no filtering of pixels (blurriness), which is
-    -- important for a nice crisp, 2D look
     love.graphics.setDefaultFilter('nearest', 'nearest')
 
-    -- set the title of our application window
     love.window.setTitle('breaKout!')
 
-    -- "seed" the RNG so that calls to random are always random
-    -- use the current time, since that will vary on startup every time
     math.randomseed(os.time())
 
-    -- initialize our nice-looking retro text fonts
     smallFont = love.graphics.newFont('font.ttf', 8)
     largeFont = love.graphics.newFont('font.ttf', 16)
     scoreFont = love.graphics.newFont('font.ttf', 32)
 
-
     love.graphics.setFont(smallFont)
 
-    -- set up our sound effects; later, we can just index this table and
-    -- call each entry's `play` method
     sounds = {
         ['hit'] = love.audio.newSource('sounds/paddle_hit.wav', 'static'),
         ['lose'] = love.audio.newSource('sounds/score.wav', 'static'),
         ['wall_hit'] = love.audio.newSource('sounds/wall_hit.wav', 'static')
     }
 
-    -- initialize window with virtual resolution
     push:setupScreen(VIRTUAL_WIDTH, VIRTUAL_HEIGHT, WINDOW_WIDTH, WINDOW_HEIGHT, {
         fullscreen = false,
         resizable = true,
         vsync = true
     })
 
-    -- initialize score variables, used for rendering on the screen and keeping
-    -- track of the score and rounds
+    -- initialize score and rounds
     playerScore = 0
     round = 1
 
-    -- initialize player paddle and ball and bricks
-    x = 30
-    player = Paddle(VIRTUAL_WIDTH/2 - 10, VIRTUAL_HEIGHT - 40, x, 5)
-    ball = Ball(VIRTUAL_WIDTH/2 - 2, VIRTUAL_HEIGHT - 45, 4, 4)
+    -- initialize player paddle and ball
+    -- make paddle smaller after ever round
+    player = Paddle(VIRTUAL_WIDTH/2 - 10, VIRTUAL_HEIGHT - 40, 35-(round*3), 5)
+    ball = Ball(player.x + (player.width / 2) - 2, player.y - 4, 4, 4)
     
-    -- Initialize table to store bricks
+    -- initialize bricks
     bricks = {}
-
-    -- Brick dimensions
     brickWidth = 50
     brickHeight = 10
-
-    -- Number of rows and columns for bricks
     rows = 4
     columns = 8
 
-    -- Create bricks and insert them into the table
+    -- add bricks to table
     for i = 1, rows do
         for j = 1, columns do
             brick = Brick(4 + (j - 1) * (brickWidth + 4), 4 + (i - 1) * (brickHeight + 4), brickWidth, brickHeight)
@@ -102,10 +71,7 @@ function love.load()
     gameState = 'start'
 end
 
---[[
-    Runs every frame, with "dt" passed in, our delta in seconds 
-    since the last frame, which LÖVE2D supplies us.
-]]
+
 function love.update(dt)
     if gameState == 'serve' then
         ball.dx = math.random(-50, 50)
@@ -114,27 +80,26 @@ function love.update(dt)
         ball.y = player.y - ball.height
 
     elseif gameState == 'play' then
-        -- detect ball collision with paddle, reversing dy if true and
-        -- slightly increasing it, then altering the dx based on the position of collision
+        
+        -- paddle collision
         if ball:collides(player) then
             ball.dy = -ball.dy
             ball.dx = math.random(-70, 70)
             sounds['hit']:play()
         end
-
-        -- detect ball collision with bricks, reversing dy if true and
-        -- slightly increasing it, then altering the dx based on the position of collision
-            for _, brick in pairs(bricks) do
-                if brick.visible and ball:collides(brick) then
-                    ball.dy = -ball.dy
-                    ball.dx = math.random(-70, 70)
-                    sounds['hit']:play()
-                    brick.visible = false
-                    playerScore = playerScore + 111
-                end
+        
+        -- brick collision
+        for _, brick in pairs(bricks) do
+            if brick.visible and ball:collides(brick) then
+                ball.dy = -ball.dy
+                ball.dx = math.random(-70, 70)
+                sounds['hit']:play()
+                brick.visible = false
+                playerScore = playerScore + 111
             end
+        end
 
-        -- detect upper screen boundary collision and reverse if collided
+        -- top screen collision
         if ball.y <= 0 then
             ball.y = 0
             ball.dy = -ball.dy
@@ -142,32 +107,25 @@ function love.update(dt)
         end
 
 
-        -- detect side boundaries and reverse if collided
+        -- side boundaries collision
         if ball.x <= 0 or ball.x >= VIRTUAL_WIDTH - 4 then
             ball.dx = -ball.dx
             sounds['hit']:play()
         end
 
 
-
-        -- if we reach the bottom of the screen, 
-        -- go back to start and update rounds
+        -- bottom screen restart and update rounds
         if ball.y >= VIRTUAL_HEIGHT then
             round = round + 1
             sounds['lose']:play()
-            player = Paddle(VIRTUAL_WIDTH/2 - 10, VIRTUAL_HEIGHT - 40, x-5, 5)
 
-            
-            -- if we've reached a 5 rounds, the game is over; set the
-            -- state to done so we can show the victory message
             if round == 6 or playerScore > 3552 then
                 gameState = 'done'
+
             else
                 gameState = 'serve'
-                -- places the ball in the middle of the screen, no velocity
                 ball:reset()
                 player:reset()
-                
             end
         end
 
@@ -192,10 +150,6 @@ function love.update(dt)
 end
 
 
---[[
-    Keyboard handling, called by LÖVE2D each frame; 
-    passes in the key we pressed so we can access.
-]]
 function love.keypressed(key)
     if key == 'escape' then
         love.event.quit()
@@ -218,15 +172,10 @@ function love.keypressed(key)
     end
 end
 
---[[
-    Called after update by LÖVE2D, used to draw anything to the screen, 
-    updated or otherwise.
-]]
+
 function love.draw()
     push:apply('start')
 
-    -- clear the screen with a specific color; in this case, a color similar
-    -- to some versions of the original Pong
     love.graphics.clear(40/255, 45/255, 52/255, 255/255)
 
     love.graphics.setFont(smallFont)
@@ -261,14 +210,14 @@ function love.draw()
         end     
         love.graphics.setColor(1,1,1,1)
 
-        -- no UI messages to display in play
     elseif gameState == 'done' then
         love.graphics.setFont(scoreFont)
+
         if playerScore < 3552 then
-            love.graphics.setColor(1,0,0,1)
+            love.graphics.setColor(76/100, 57/100, 63/100, 1)
             love.graphics.printf('You did not destroy all of the bricks.\nBetter luck next time!', 0, 50, VIRTUAL_WIDTH, 'center')
         else
-            love.graphics.setColor(0,1,0,1)
+            love.graphics.setColor(157/255, 194/255, 9/255, 1)
             love.graphics.printf('You destroyed all of the bricks!\nYou are SO AMAZING!', 0, 50, VIRTUAL_WIDTH, 'center')
         end    
         love.graphics.setFont(smallFont)
@@ -287,13 +236,12 @@ function love.draw()
     player:render()
     ball:render()
     
-    
 
     push:apply('end')
 end
 
 function displayScore()
-    love.graphics.setColor(0, 1, 0, 1)
+    love.graphics.setColor(157/255, 194/255, 9/255, 1)
     love.graphics.setFont(scoreFont)
     love.graphics.print(tostring(playerScore), 10, VIRTUAL_HEIGHT - 50)
     love.graphics.setColor(1, 1, 1, 1)
